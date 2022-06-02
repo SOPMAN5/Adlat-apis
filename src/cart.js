044b28a0a9101314ab855b12ad8772e29ad84040
jonas <jonas@plclub.se>
2022-06-02 09:48:51 +0100

    updates api.js

--- src/cart.js
+++ src/cart.js
@@ -84,8 +84,8 @@ handler.validCart = (products,cartItems,selectedPercent,res) => {
    console.log(totalPrice)
   if(totalPrice){
       const validOption = [25,50,100];
-     selectedPercent = validOption.includes(selectedPercent) ?selectedPercent:100
-    const amountToPay = totalPrice *(selectedPercent/100)
+     selectedPercent = validOption.includes(NUmber(selectedPercent)) ?Number(selectedPercent):100
+    const amountToPay = totalPrice *(Number(selectedPercent)/100)
     let token = jwt.sign({
         data: amountToPay
       }, process.env.Secret_Key, { expiresIn: 60 * 3 });
